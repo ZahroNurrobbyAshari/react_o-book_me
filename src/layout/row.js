49529927f67e63b5ac4fbef43c727a68091d875d
Zahro Nurrobby Ashari <zahronurrobbya@gmail.com>
2021-05-21 04:54:27 +0700

    memperbaiki row

--- src/layout/row.js
+++ src/layout/row.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, {  useState, useEffect } from "react";
 import axios from "axios";
 import "@splidejs/splide/dist/css/themes/splide-default.min.css";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
